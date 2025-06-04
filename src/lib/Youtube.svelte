a46c8b42f07bc68e7f8467a307aa9e04ca07b486
sierra-95 <michaelmachohi@gmail.com>
2025-06-04 22:01:02 +0300

    First package

---
+++ src/lib/Youtube.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	let { uid, title }: { uid: string; title: string } = $props();
+</script>
+<iframe
+	src="https://www.youtube.com/embed/{uid}"
+	{title}
+	frameborder="0"
+	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+	allowfullscreen
+></iframe>
