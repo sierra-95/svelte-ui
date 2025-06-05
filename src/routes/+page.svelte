afd6c372e8086be61f7439c8007f1836993727e3
sierra-95 <michaelmachohi@gmail.com>
2025-06-06 00:47:11 +0300

    docs setup

--- src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import {LinearProgress, CircularProgress,Button} from '$lib/index.js'
-
-</script>
-
-<LinearProgress/>
-<CircularProgress/>
-<Button externalClass='ml-2'>Button</Button>
