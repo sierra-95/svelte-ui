394e219e22b4409d15f1c504199ee3bb5e1a62a5
sierra-95 <michaelmachohi@gmail.com>
2025-07-22 00:32:44 +0300

    Getting started page upgrade with shiki

--- src/routes/docs/+page.svelte
+++ src/routes/docs/+page.svelte
@@ -11,7 +11,7 @@
 <main>
   <title>Getting Started</title>
   <p>
-    <strong class="text-[#334155]">@sierra-95/svelte-ui</strong> is a modern crafted UI component library built for the Svelte ecosystem.  
+    <strong class="text-[#334155]">@sierra-95/svelte-ui</strong> is a component library built for the Svelte ecosystem.  
     Designed as a solid foundation for developers, it offers expressive, accessible, and flexible components to accelerate your Svelte projects.
   </p>
   <p>
