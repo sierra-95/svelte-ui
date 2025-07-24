e876d0191456dec82a57413831cb46b98119ebcb
sierra-95 <michaelmachohi@gmail.com>
2025-07-24 22:30:27 +0300

    Component Button Docuumentation

--- src/routes/svelte/components/button/+page.svelte
+++ src/routes/svelte/components/button/+page.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
 	import {Button} from '$lib/index.js'
 	import {RenderCode, ButtonTable} from '../../../../components/index.js';
+	const contained_buttons = [
+		{ color: 'primary', title: 'Primary contained' },
+		{ color: 'secondary', title: 'Secondary contained' },
+		{ color: 'error', title: 'Error contained' }
+	];
+	const outlined_buttons = [
+		{ color: 'primary', title: 'Primary outlined' },
+		{ color: 'secondary', title: 'Secondary outlined' },
+		{ color: 'error', title: 'Error outlined' }
+	];
 </script>
 
 <main class='flex flex-col gap-4'>
@@ -17,14 +27,14 @@
 		</Button>
 	`}/>
 	<div class="flex gap-4">
-		<Button variant='contained' title="Primary contained">Button</Button>
-		<Button variant='contained' color='secondary' title="Secondary contained">Button</Button>
-		<Button variant='contained' color='error' title="Error contained">Button</Button>
+	{#each contained_buttons as { color, title }}
+		<Button variant="contained" color={color} title={title}>Button</Button>
+	{/each}
 	</div>
 	<div class="flex gap-4">
-		<Button variant='outlined' title="Primary outlined">Button</Button>
-		<Button variant='outlined' color='secondary' title="Secondary outlined">Button</Button>
-		<Button variant='outlined' color='error' title="Error outlined">Button</Button>
+	{#each outlined_buttons as { color, title }}
+		<Button variant="outlined" color={color} title={title}>Button</Button>
+	{/each}
 	</div>
 	<div class="flex gap-4">
 		<Button variant='contained' isLoading title="Loading"></Button>
@@ -33,8 +43,7 @@
 	</div>
 	<div class='flex flex-col gap-2'>
 		<h2 class='mt-4 text-xl'>This is a custom button. You can customize using Tailwind css using the <strong>style</strong> prop.</h2>
-		<Button title="Custom Button" style="w-[200px] h-[100px] bg-purple-500">Button</Button>
-		<Button title="Custom Button" style="w-[100px] bg-black" isLoading spinner={50} thickness={10}>Button</Button>
+		<Button title="Custom Button" style="w-[200px] bg-black" isLoading spinner={50} thickness={10}>Button</Button>
 		<RenderCode
 			lang="svelte"
 			code={`
