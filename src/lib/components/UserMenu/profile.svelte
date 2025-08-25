7ecfa41f876610c24bb63ae82aa4387b926b211a
sierra-95 <michaelmachohi@gmail.com>
2025-08-25 14:38:16 +0300

    User Menu complete

--- src/lib/components/UserMenu/profile.svelte
+++ src/lib/components/UserMenu/profile.svelte
@@ -1,27 +1,28 @@
-<script>
+<script lang="ts">
   import { fade, fly } from 'svelte/transition';
   import { quintOut } from 'svelte/easing';
 
-  export let onProfile = () => {};
-  export let onLogout = () => {};
-  export let iconBg = '#3b82f6';
-  /**
-   * @type {{ name?: string; email?: string } | null}
-  */
-  export let user = null;
+  export let onProfile: () => void = () => {};
+  export let onLogout: () => void = () => {};
+  export let iconBg: string = '#3b82f6';
+  export let absolute: 'right' | 'left' = 'left';
 
-  let anchorEl = null;
-  let open = false;
-  
-  const handleClick = (/** @type {{ currentTarget: any; }} */ event) => {
+  type User = { name?: string; email?: string } | null;
+  export let user: User = null;
+
+  let anchorEl: HTMLElement | null = null;
+  export let open: boolean = false; // Just for display purposes (export)
+
+  const handleClick = (event: { currentTarget: HTMLElement }) => {
     anchorEl = event.currentTarget;
     open = !open;
   };
-  
+
   const handleClose = () => {
     open = false;
   };
 </script>
+
 
 <style>
   @import './profile.css';
@@ -34,17 +35,17 @@
   
   {#if open}
     <div class="menu-backdrop" role="none" on:click|self={handleClose} transition:fade={{ duration: 150 }}>
-      <div class="menu-paper" transition:fly={{ y: -10, duration: 200, easing: quintOut }}>
+      <div class="menu-paper {absolute==='right'? 'right-0':'left-0'}" transition:fly={{ y: -10, duration: 200, easing: quintOut }}>
         <div class="p-4 border-b-1 border-gray-100 text-left">
           <h2>{user?.name ?? 'User\'s Name'}</h2>
           <p class="text-[0.9rem] text-[#666]">{user?.email ?? 'Email'}</p>
         </div>
         
         <ul class="menu-list">
-            <a href='/dashboard/settings/profile' class="menu-item" on:click={() => {handleClose(); onProfile()}}>
+            <li role="none" class="menu-item" on:click={() => {handleClose(); onProfile()}}>
               <i style="--iconBg: {iconBg}" class="fa-solid fa-user"></i>
               <span class="ml-4">Profile</span>
-            </a>
+            </li>
             <li role="none" class="menu-item" on:click={() => onLogout()}>
               <i style="--iconBg: {iconBg}" class="fa-solid fa-right-from-bracket"></i>
               <span class="ml-4">Logout</span>
--- src/routes/components/user/+page.svelte
+++ src/routes/components/user/+page.svelte
@@ -1,5 +1,60 @@
 <script>
     import {UserMenu} from '$lib/index.js'
+	import {RenderCode} from '../../../components/index.js';
+    let open = $state(true)
+
+    let user = {
+        name: 'John Doe',
+        email: 'john.doe@example.com'
+    };
 </script>
 
-<UserMenu/>
+<title>User Menu</title>
+
+<h1 class="font-bold text-2xl">User Menu</h1>
+<h2>
+  This menu is built to be used in portals as a developer's quick tool.
+  For further customization, refer to the
+  <a class="text-blue-500"
+    target="_blank"
+    href="https://github.com/sierra-95/svelte-ui/blob/main/src/lib/components/UserMenu/profile.svelte">GitHub code</a>.
+</h2>
+<UserMenu bind:open />
+{#if open}
+    <div class="h-[250px]"></div>
+{/if}
+<RenderCode
+	lang="svelte"
+	code={`
+	<\script>
+        import { goto } from '$app/navigation';
+		import { UserMenu } from '@sierra-95/svelte-ui';
+
+        let user = {
+            name: 'John Doe',
+            email: 'john.doe@example.com'
+        };
+
+        function handleLogout(){
+          // Handle logout logic here
+        }
+        function handleProfile(){
+          // Handle profile logic here
+          goto('/settings/profile')
+        }
+	<\/script>
+    <div class="div w-full flex items-end justify-end">
+        <!--By default, UserMenu dropdown is absolute left of the parent
+        If you need it on the right, set absolute='right'
+        Like the example below-->
+        <!--This is usefull if you need usermenu on the right,
+        but dropdown on its left-->
+	    <UserMenu
+            user={user}
+            iconBg='#10b981'
+            absolute='right' 
+            onProfile={handleProfile} 
+            onLogout={handleLogout}
+        />
+    </div>
+`}/>
